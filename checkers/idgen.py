772e3ceb6041f7647af8d68f85675b3dbd6dc84b
Brent Atkinson <brent.atkinson@gmail.com>
2014-09-25 19:17:34 -0400

    Reformatted id generation code.

--- checkers/idgen.py
+++ checkers/idgen.py
@@ -1,12 +1,13 @@
 from random import choice
 
-with open('nouns.txt','r') as nfile:
-   nouns = map(str.strip, nfile.readlines())
+with open('nouns.txt', 'r') as nfile:
+    nouns = map(str.strip, nfile.readlines())
 
-with open('adjectives.txt','r') as afile:
-   adjs = map(str.strip, afile.readlines())
+with open('adjectives.txt', 'r') as afile:
+    adjectives = map(str.strip, afile.readlines())
+
 
 def gen_id():
-   return '_'.join([choice(adjs), choice(nouns)])
+    return '_'.join([choice(adjectives), choice(nouns)])
 
 
